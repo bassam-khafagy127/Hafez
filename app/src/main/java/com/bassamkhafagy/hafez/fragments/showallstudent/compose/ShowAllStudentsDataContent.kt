f2b680c918f026ce3bdab7379576f74f34ee4c20
bassam nasser <58662195+bassam-khafagy127@users.noreply.github.com>
2023-11-11 00:33:54 +0200

    best state

--- app/src/main/java/com/bassamkhafagy/hafez/fragments/showallstudent/compose/ShowAllStudentsDataContent.kt
+++ app/src/main/java/com/bassamkhafagy/hafez/fragments/showallstudent/compose/ShowAllStudentsDataContent.kt
@@ -2,9 +2,14 @@ package com.bassamkhafagy.hafez.fragments.showallstudent.compose
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.bassamkhafagy.hafez.R
 import com.bassamkhafagy.hafez.data.local.ImportedData
@@ -25,10 +30,12 @@ fun ShowAllStudentsDataContent(studentUIState: StudentUIState) {
             }
 
             is StudentUIState.Success -> {
-                LazyColumn {
+                LazyColumn(contentPadding = PaddingValues(horizontal = 6.dp)) {
                     stickyHeader {
                         StudentListHeader()
                     }
+                    item { Spacer(modifier = Modifier.height(32.dp)) }
+
                     itemsIndexed(studentUIState.students) { index: Int, student: ImportedData ->
                         StudentItem(
                             studentName = student.studentsName.toString(),
