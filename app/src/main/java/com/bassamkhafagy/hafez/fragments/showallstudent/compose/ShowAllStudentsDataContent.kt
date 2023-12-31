cfa178d0c97fceeb180c486656a07d5c9fd199d2
BassamKhafagy <bassam.nasser@megatrust.co>
2023-12-31 16:35:26 +0200

    validation

--- app/src/main/java/com/bassamkhafagy/hafez/fragments/showallstudent/compose/ShowAllStudentsDataContent.kt
+++ app/src/main/java/com/bassamkhafagy/hafez/fragments/showallstudent/compose/ShowAllStudentsDataContent.kt
@@ -2,19 +2,15 @@ package com.bassamkhafagy.hafez.fragments.showallstudent.compose
 
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
-import com.bassamkhafagy.hafez.R
 import com.bassamkhafagy.hafez.data.local.ImportedData
-import com.bassamkhafagy.hafez.data.local.SoraReview
-import com.bassamkhafagy.hafez.data.local.Student
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
@@ -26,7 +22,7 @@ fun ShowAllStudentsDataContent(studentUIState: StudentUIState) {
             }
 
             StudentUIState.Loading -> {
-
+            CircularProgressIndicator()
             }
 
             is StudentUIState.Success -> {
--- app/src/main/java/com/bassamkhafagy/hafez/util/ValidateReviewChecker.kt
+++ app/src/main/java/com/bassamkhafagy/hafez/util/ValidateReviewChecker.kt
@@ -9,13 +9,15 @@ fun checkReview(review: SoraReview, context: Context): RegisterValidation {
     if (review.studentCode!!.isEmpty()) {
         return RegisterValidation.Failed(context.getString(R.string.studentWarning))
     }
-    if (review.sheikhName!!.isEmpty()) {
-        return RegisterValidation.Failed(context.getString(R.string.shikhWarning))
-    }
 
-    if (review.ring!!.isEmpty()) {
-        return RegisterValidation.Failed(context.getString(R.string.ringWarning))
-    }
+//    if (review.sheikhName!!.isEmpty()) {
+//        return RegisterValidation.Failed(context.getString(R.string.shikhWarning))
+//    }
+
+//    if (review.ring!!.isEmpty()) {
+//        return RegisterValidation.Failed(context.getString(R.string.ringWarning))
+//    }
+
     if (review.soraName!!.isEmpty()) {
         return RegisterValidation.Failed(context.getString(R.string.soraWarning))
     }
